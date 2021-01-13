a957585a41067e83638418985fa83345a0c32b30
juthikakarmakar <juthikakarmakar1@gmail.com>
2021-01-13 22:53:03 +0530

    First commit

---
+++ src/main/java/ProjectExecutionTypes/Test1/App.java
@@ -0,0 +1,13 @@
+package ProjectExecutionTypes.Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
